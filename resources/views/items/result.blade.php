b253b3f83b52ead01eefdfbf101229a213aeb564
mantorn <m.gajewski@connectto.pl>
2024-05-10 23:32:56 +0200

    v1.0

--- resources/views/items/result.blade.php
+++ resources/views/items/result.blade.php
@@ -6,14 +6,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Item search result</div>
-                    {{$errors}}
-                    @if($errors->any())
-                        <div class="row d-flex text-center">
-                            <ul>
-                            {{ implode('', $errors->all('<li>:message</li>')) }}
-                            </ul>
-                        </div>   
-                    @endif
                     <div class="row d-flex mb-3 text-center">
                         <a class="col-6" href="{{route('items.one.find', $item->id-1)}}">PREV</a>
                         <a class="col-6" href="{{route('items.one.find', $item->id+1)}}">NEXT</a>
